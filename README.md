44b01fa79f8953d83053c205f8a37a51092133eb
Kris <109919530+maleker86@users.noreply.github.com>
2023-12-31 12:01:19 -0800

    Add in Board, no Win State or Player Default Name.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tic-tac-toe
 This project was completed on Replit.com prior to its repo creation. 
-- Initial setup: HTML, CSS, JS. Html with player buttons and a gameboard. 
+- The Board responds to player input. Player space is blank when not provided. No Win State??
--- script.js
+++ script.js
@@ -1,91 +1,136 @@
-// const playerController = (playerChoice) => {
-//   const displayChoice = () => console.log(`The player chose ${playerChoice}`);
-
-//   return { 
-//     playerChoice,
-//     displayChoice 
-//   };
-// };
-
-// const playerOne = playerController("x");
-// const playerTwo = playerController("o");
-
-// const Game = () => {
-
-// }
-
-// const GameBoard = () => {
-//   let gameBoard = [
-//     ['.', '.', '.'],
-//     ['.', '.', 'x'],
-//     ['.', '.', '.']
-//   ];
-
-//   let gameBoardShitty = {".",".",".",
-//                         "o",".",".",
-//                         ".",".","."};
-
-
-//   return {gameBoardShitty};
-// }
-
-// console.log(GameBoard()){};
-
-const GameBoard = function() {
-
-    const Intialize = () => {
-      let choices = [
-        'x', 'o', 'x',
-        'o', 'x', 'o',
-        'o', 'o', 'x',
-      ];
+// 26 mins into https://www.youtube.com/watch?v=kVE4xX-OkJo 
+const GameBoard = (() => {
   
-      return choices;
-    }
-  
-    const DisplayGame = () => {
-      let ShowGameBoard = Initialize();
-  
-      for (let i = 0; i < ShowGameBoard.length; i++) {
-        let gameboard = document.querySelector("#gameboard");
-        let box = document.createElement("div");
-  
-        gameboard.append(box);
-        box.innerText = (ShowGameBoard[i]);
-        // console.log(ShowGameBoard[i]);
-      };
+    let gameboard = ['','','',
+                    '','','',
+                    '','','',];
+    
+    const renderBoard = () => {
+      let boardHTML = "";
+      gameboard.forEach((box, index) => {
+        boardHTML += `<div class="box" onclick="Game.handleClick(${index})" data-number-type="${index}">${box}</div>`;
+      }); 
+      document.getElementById("board").innerHTML = boardHTML; 
     };
   
-    return { Initialize, DisplayGame };
-  }();
-  
-  
-  
-  let Player = () => {
-    // let button = document.querySelector("button");
-    // button.addEventListener("click", Click);
-  
-    // function Click() {
-    //   console.log(button.innerText);
-    // }
-  
-    let clicky = document.getElementById("gameboard").querySelectorAll('div');
-  
-    clicky.onclick = function() {
-      Hello('hi');
-    }
-  
-    function Hello(text) {
-      console.log(text);
-    }
-  
-    // clicky.onclick = function() {Hi()};
-  
-    function Hi() {
-      console.log("hi");
+    // omg 16 - 27 is literally a programming issue, come back later xD 
+    const freeSquare = (item) => {
+      if (item === "") {
+        // console.log("open space found");
+        // console.log(item);
+        return true;
+      } else {
+        // console.log("this space isn't open");
+        // console.log(item);
+        return false;
+      }
     };
+    
+    const update = (index, mark) => {  
+        // console.log(`the gameboard says ` + gameboard[index]);
+        // console.log(`the spot ${index} has a ${mark}`);
+        gameboard[index] = mark;
+        // console.log(index, value);
+        // console.log(gameboard);
+        renderBoard();
+    };
+  
+    const changeMsg = (text) => {
+      // console.log("This works rn");
+      let msg = document.getElementById("message");
+      msg.textContent = `${text}`;
+    };
+  
+    return {
+      gameboard,
+      freeSquare,
+      renderBoard,
+      update,
+      changeMsg,
+    }
+  })();
+  
+  const createPlayer = (name, mark) => {
+    return {
+      name,
+      mark,
+    }
   };
   
-  GameBoard.Initialize();
-  GameBoard.DisplayGame();
-  // GameBoard.DisplayGame();
+  const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+    
+    const gameStart = () => {
+      players = [
+        createPlayer(document.querySelector("#player1").value, "x"),
+        createPlayer(document.querySelector("#player2").value, "o"),
+      ]
+      currentPlayerIndex = 0;
+      gameOver = false;
+      GameBoard.renderBoard();
+      GameBoard.changeMsg(`the current player is ${players[currentPlayerIndex].name}. they are ${players[currentPlayerIndex].mark}`);
+    };
+  
+    const restartGame = () => {
+      for (i = 0; i < 9; i++) {
+        GameBoard.update(i, "");
+      }
+      GameBoard.renderBoard();
+      gameStart();
+    }
+    
+    const handleClick = (index) => {
+      // console.log("Hi");
+      // let index = box.getAttribute("data-number-type");
+      // console.log(`the index is ` + index);
+      //i want to stop the index update Here but I don't Think that I Can :) Sorry! 
+      
+      if (!GameBoard.freeSquare(GameBoard.gameboard[index])) {
+        return;
+      };
+      GameBoard.update(index, players[currentPlayerIndex].mark);
+      Game.playerSelector();
+    };
+  
+    const playerSelector = () => {    
+      if (currentPlayerIndex === 0) {
+      GameBoard.changeMsg(`the previous player is ${players[currentPlayerIndex].name}. they are ${players[currentPlayerIndex].mark}`);
+      currentPlayerIndex = 1;
+      // console.log(`the previous player is ${currentPlayerIndex}`);
+      // console.log(`current player index is ${currentPlayerIndex}`);
+        GameBoard.changeMsg(`the current player is ${players[currentPlayerIndex].name}. they are ${players[currentPlayerIndex].mark}`);
+        // console.log(`it is next the turn of ` + players[currentPlayerIndex].name);
+      } else {
+      GameBoard.changeMsg(`the previous player is ${players[currentPlayerIndex].name}. they are ${players[currentPlayerIndex].mark}`);
+      currentPlayerIndex = 0;
+      // console.log(`the previous player is ${currentPlayerIndex}`);
+      // console.log(`current player index is ${currentPlayerIndex}`);
+        GameBoard.changeMsg(`the current player is ${players[currentPlayerIndex].name}. they are ${players[currentPlayerIndex].mark}`);
+        // console.log(`it is next the turn of ` + players[currentPlayerIndex].name);
+      };
+      return {currentPlayerIndex};
+    }; 
+    
+    return {
+      gameStart, 
+      restartGame,
+      handleClick, 
+      playerSelector, 
+    };
+  })();
+  
+  // UI rendered stuff
+  const startButton = document.getElementById("start");
+  startButton.addEventListener("click", () => {
+    Game.gameStart();
+    startButton.style.display = "none";
+  });
+  
+  const restartButton = document.getElementById("restart");
+  restartButton.addEventListener("click", () => {
+    Game.restartGame();
+    console.log("Made it this far kid. Game Restarted.")
+    startButton.style.display = "block";
+  });
